397ec38deaf8368e26c0468f35070cfe61f0abf0
Monika Girdhar <monikagirdhar9676@gmail.com>
2024-07-14 15:38:49 +0530

    git config dev

--- first.js
+++ first.js
@@ -1,4 +1,27 @@
 var x=30;
 console.log(x);
+
 let y="Monika"
 console.log(y);
+
+const bool=true;
+console.log(bool);
+
+//typeof operator-data type
+var a=10;
+var b="Girdhar";
+var c=false;
+var d=[a,b,c];
+var e={name:"Monika",class:"MCA"};
+console.log(`${typeof(a)},${typeof(b)},${typeof(c)},${typeof(d)},${typeof(e)}`);
+
+//Reassigning
+let t="monika";
+console.log(t);
+t="Girdhar";
+console.log(t);
+
+//understanding const- can't reassign to constant
+const m="monika";
+//m="Girdhar";
+console.log(m);
